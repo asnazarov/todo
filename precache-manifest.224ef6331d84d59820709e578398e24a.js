858bb295497da87c4fd273f31b556d8e8c65bec2
Alexander Nazarov <asn.nazarov.asn@gmail.com>
2020-10-25 15:47:30 +0300

    Updates

---
+++ precache-manifest.224ef6331d84d59820709e578398e24a.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5fd9a78b3807f61b047a581d41e1ecd4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "71618cb13de6e1424521",
+    "url": "/static/css/main.80f2641c.chunk.css"
+  },
+  {
+    "revision": "d955f6f31719248a0897",
+    "url": "/static/js/2.95132750.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.95132750.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "71618cb13de6e1424521",
+    "url": "/static/js/main.4ca49dac.chunk.js"
+  },
+  {
+    "revision": "00902ced120ce6c3e601",
+    "url": "/static/js/runtime-main.285bc84b.js"
+  },
+  {
+    "revision": "a5b017a6d056e351caaef66c63f8d814",
+    "url": "/static/media/addTask.a5b017a6.svg"
+  },
+  {
+    "revision": "14e2f379ea2f16993535086cc7b87b36",
+    "url": "/static/media/close.14e2f379.svg"
+  },
+  {
+    "revision": "8b2f00f770036eafa52af0660b327a2d",
+    "url": "/static/media/edit.8b2f00f7.svg"
+  },
+  {
+    "revision": "a4124f13c2c013867b99ba418bbc6c36",
+    "url": "/static/media/remove.a4124f13.svg"
+  }
+]);
